d344debdf7455df2ce017b550720ee38d3422b7b
Glowdragon <info@glowdragon.de>
2020-11-07 15:50:59 +0100

    Dashen funktioniert, noch nicht finetuned

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,9 +12,18 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]
     private Transform model = default;
+
+    [SerializeField]
+    private Collider2D collider;
     
     [SerializeField]
     private GroundChecker groundChecker = default;
+    
+    [SerializeField]
+    private LayerMask playerLayer;
+
+    [SerializeField]
+    private LayerMask platformLayer;
     
     [Header("Movement")]
     [SerializeField]
@@ -36,6 +45,9 @@ public class PlayerController : MonoBehaviour
     
     [SerializeField]
     private float jumpVelocity;
+
+    [SerializeField]
+    private float dashTime;
 
     [SerializeField]
     private float jumpInputAhead;
@@ -62,7 +74,9 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput = 0;
     private float jumpInputTimeout = 0;
     private bool dropInput = false;
+    private bool dashInput = false;
 
+    private bool ignorePlatforms = false;
     private int extraJumpsUsed = 0;
     
     // Start is called before the first frame update
@@ -89,6 +103,11 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Drop"))
         {
             this.dropInput = true;
+        }
+
+        if (Input.GetButtonDown("Dash"))
+        {
+            this.dashInput = true;
         }
 
         if (this.groundChecker.touchingGround)
@@ -165,7 +184,11 @@ public class PlayerController : MonoBehaviour
             bool canJumpInAir = this.extraJumpsUsed < this.extraJumps;
             if (canJumpFromGround || canJumpInAir)
             {
-                if (!canJumpFromGround)
+                if (canJumpFromGround)
+                {
+                    this.extraJumpsUsed = 0;
+                }
+                else
                 {
                     this.extraJumpsUsed++;
                 }
@@ -180,23 +203,18 @@ public class PlayerController : MonoBehaviour
                 {
                     targetVel.x = -3;
                 }
+                
                 this.rb.velocity = new Vector2(this.rb.velocity.x, 0);
-
-                var myPos = this.transform.position;
-                var mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
-                Debug.Log(mousePos + " vorher");
-                mousePos.z = myPos.z;
-                Debug.Log(mousePos + " nachher");
-
-                //Object.Instantiate(this.testPrefab, mousePos, Quaternion.identity);
-
-                var jumpDir = (mousePos - myPos).normalized;
-       
-                //Debug.Log("Jump Direction: " + jumpDir);
-
-                this.rb.AddForce(jumpDir * this.jumpVelocity, ForceMode2D.Impulse);
+                this.rb.AddForce(Vector2.up * this.jumpVelocity, ForceMode2D.Impulse);
             }
         }
+        
+        // Dashing
+        if (this.dashInput)
+        {
+            this.StartCoroutine(this.CDash());
+        }
+        this.dashInput = false;
 
         // Higher gravity when falling
         if (this.rb.velocity.y < 0)
@@ -217,6 +235,48 @@ public class PlayerController : MonoBehaviour
         //this.rb.velocity = targetVel;
         this.rb.velocity = Vector2.SmoothDamp(this.rb.velocity, targetVel, ref this.velocity, this.movementSmoothing);
     }
+
+    private IEnumerator CDash()
+    {
+        Debug.Log("Dash");
+        
+        this.rb.gravityScale = 0.2f;
+        
+        var myPos = this.transform.position;
+        var mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+        mousePos.z = myPos.z;
+        var jumpDir = (mousePos - myPos).normalized;
+        this.rb.AddForce(jumpDir * this.jumpVelocity, ForceMode2D.Impulse);
+
+        foreach (Platform platform in Object.FindObjectsOfType<Platform>())
+        {
+            platform.GetComponent<BoxCollider2D>().enabled = false;
+        }
+        
+        yield return new WaitForSeconds(this.dashTime);
+        
+        foreach (Platform platform in Object.FindObjectsOfType<Platform>())
+        {
+            platform.GetComponent<BoxCollider2D>().enabled = true;
+        }
+        
+        this.rb.gravityScale = 1;
+    }
+
+    /*private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (this.ignorePlatforms && other.collider.GetComponent<Platform>() != null)
+        {
+            Physics2D.IgnoreCollision(this.collider, other.collider, true);
+            this.StartCoroutine(this.CEnableCollision(other.collider));
+        }
+    }
+
+    private IEnumerator CEnableCollision(Collider2D otherCollider)
+    {
+        yield return new WaitForSeconds(this.dashTime);
+        Physics2D.IgnoreCollision(this.collider, otherCollider, false);
+    }*/
 
     void OnGUI()
     {
